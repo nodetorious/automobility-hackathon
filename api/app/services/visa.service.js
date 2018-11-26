9ab4768d420e67ff0d0f2763b7509a744f20e36a
Andrew Janzen <a.s.janzen@gmail.com>
2018-11-26 07:33:34 -0800

    visa merchant locator endpoint

--- api/app/services/visa.service.js
+++ api/app/services/visa.service.js
@@ -1,4 +1,3 @@
-// const axios = require('axios');
 const https = require('https');
 const request = require('request');
 const req = request.defaults();
@@ -13,26 +12,7 @@ class VisaService {
     const userId = process.env.VISA_USERID;
     const password = process.env.VISA_PASSWORD;
 
-    // return axios.request({
-    //   method: 'post',
-    //   // url: 'https://sandbox.api.visa.com/merchantlocator/v1/locator',
-    //   url: 'https://sandbox.api.visa.com/merchantsearch/v1/search',
-    //   auth: {
-    //     username: userId,
-    //     password: password
-    //   },
-    //   data: body,
-    //   httpsAgent: new https.Agent({
-    //     cert: fs.readFileSync(certificateFile),
-    //     key: fs.readFileSync(keyFile),
-    //     // auth: 'Basic ' + new Buffer(userId + ':' + password).toString('base64')
-    //     // auth: 'user:password',
-    //     // rejectUnauthorized: false
-    //   })
-    // });
-
     return req.post({
-      // uri: 'https://sandbox.api.visa.com/merchantlocator/v1/locator',
       uri: 'https://sandbox.api.visa.com/merchantsearch/v1/search',
       key: fs.readFileSync(keyFile),
       cert: fs.readFileSync(certificateFile),
@@ -42,14 +22,30 @@ class VisaService {
         'Authorization': 'Basic ' + new Buffer(userId + ':' + password).toString('base64')
       },
       body: JSON.stringify(body),
-      // body: body,
     }, (error, response, body) => {
       onSuccess(body);
-      // console.log(response);
-      // console.error(error);
     });
+  }
 
+  static merchantLocator(body, onSuccess) {
+    const keyFile = path.join(__dirname, '..', '..', 'secrets', 'key_8f8104b5-b24a-4007-9f88-a0797d1d8ce7.pem');
+    const certificateFile = path.join(__dirname, '..', '..', 'secrets', 'cert.pem');
+    const userId = process.env.VISA_USERID;
+    const password = process.env.VISA_PASSWORD;
 
+    return req.post({
+      uri: 'https://sandbox.api.visa.com/merchantlocator/v1/locator',
+      key: fs.readFileSync(keyFile),
+      cert: fs.readFileSync(certificateFile),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Basic ' + new Buffer(userId + ':' + password).toString('base64')
+      },
+      body: JSON.stringify(body),
+    }, (error, response, body) => {
+      onSuccess(body);
+    });
   }
 }
 
